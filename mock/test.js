1a3dfd9a4610141d2201bdc556528628b6cc144f
monkeyfeiyu <136320943@qq.com>
2019-01-08 14:10:11 +0800

    m

--- mock/test.js
+++ mock/test.js
@@ -1,9 +1,2 @@
-const axios = require('axios');
-
 module.exports = {
-    async registry(ctx) {
-        return await axios.get('https://registry.npm.taobao.org/' + ctx.query.name).then(res => {
-            return res.data
-        });
-    }
 };
