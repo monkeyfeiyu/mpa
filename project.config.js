8295f9c1354330cda6934b644a976103b9cccf5e
monkeyfeiyu <136320943@qq.com>
2019-05-06 15:30:23 +0800

    Update project.config.js

--- project.config.js
+++ project.config.js
@@ -16,12 +16,29 @@ module.exports = {
                     headers
                 }
             } = ctx;
-            if (path.startsWith('/interface')) {
-                const data = await fetch('http://192.168.1.1' + path, {
-                    body: JSON.stringify(body),
-                    method: 'POST',
+            if (path.startsWith('/api')) {
+                const getConfig = {
+                    method: method,
+                    headers: headers,
+                    rejectUnauthorized: false
+                };
+                const postConfig = {
+                    body: qs.stringify(body),
+                    method: method,
                     headers: headers
-                }).then(res => res.json());
+                };
+
+                const isGet = method.toLowerCase() === 'get';
+                const fetchParams = isGet ? getConfig : postConfig;
+                const urlPrefix = 'http://192.168.1.1';
+
+                let url = [urlPrefix, path].join('');
+
+                if (isGet) {
+                    url = [url, qs.stringify(query)].join('?');
+                }
+
+                const data = await fetch(url, fetchParams).then(res => res.json());
                 return ctx.body = data;
             }
             return await next();
